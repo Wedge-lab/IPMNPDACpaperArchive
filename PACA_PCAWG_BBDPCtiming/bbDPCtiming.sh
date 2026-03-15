e1077bf3a222e823dec42df8076ee92ffc7a5458
XiaotongHe <xiaotonghem36uk@gmail.com>
2026-03-15 17:22:25 +0000

    Update bbDPCtiming.sh

--- PACA_PCAWG_BBDPCtiming/bbDPCtiming.sh
+++ PACA_PCAWG_BBDPCtiming/bbDPCtiming.sh
@@ -1,5 +1,5 @@
 #!/bin/bash --login
-#SBATCH -J bbtmpaca
+#SBATCH -J bbtmtest
 #SBATCH -p multicore
 #SBATCH -n 2
 #SBATCH -t 2-0
@@ -7,16 +7,18 @@
 #SBATCH -o /yourFolder/logs/%A_%a.out
 #SBATCH -e /yourFolder/logs/%A_%a.err
 
+set -euo pipefail
+
 module purge
 conda activate xtpy311
 
 # Get the Nth line from a file based on array task ID
-sampleID=`sed -n "${SLURM_ARRAY_TASK_ID}p" <(awk '{ print $1 }' sampleList.txt)`
+sampleID=$(awk 'NR==ENVIRON["SLURM_ARRAY_TASK_ID"] {print $1}' samplelist.txt)
 ssDPCIfolderpath='/Data/dpcInputs'
 dpcoutfolderpath='/Data/dpcOutputs/'
 outpath4in='/Data/timingOutputs/'
 
-python3 ./bbDPCtiming.py ${sampleID} ${ssDPCIfolderpath} ${dpcoutfolderpath} ${outpath4in}
+python3 bbdpctiming.py "$sampleID" "$ssDPCIfolderpath" "$dpcoutfolderpath" "$outpath4in"
 
 
 
